52f1a76159c0ca27f6fba68447a39a01ebe0caa6
xtao17 <59926947+xtao17@users.noreply.github.com>
2020-04-07 15:43:22 -0400

    Add files via upload

--- lexer.py
+++ lexer.py
@@ -1,94 +1,72 @@
 import sys
 from typing import Generator, Tuple
 import re
-
-"""
-#.* - search for a comment
-\s*#.* - comments with whitespce in front including new lines
-^  - matches the start of the line
-^[ \t]*#.* - look for comments that start a line 
-^[ \t]*#.*$ - match single line comment
-. - matches any character
-cl.[ds] - matches clud and clas
-
-Replace single line comments with a triple quoted string version
-
-^[ \t]*#(.*) - single line comment but capture the comment content
-            parentheses in a regular expression represent a capture group
-            
-[_a-zA-Z][_a-zA-Z0-9]* - match an identifier
-\w - shorthand for [_a-zA-Z0-9]
-[_a-zA-Z]\w*
-
-What are the rules for an integer constant?
-[0-9]+ - integer constant
-0[xX][0-9a-fA-F]+ - hexadecimal constant
-"""
-
-# """\n$1\n""" - replacement text box where $1 represents the captured value
-
-"""
- sample comment
-"""
-
-"""
-sample comment
-"""
-
 class Lexer:
-    # class variables
-    INTLIT  = 0  # 1. setattr builtin function
-    PLUS    = 1  # 2. named tuple
-    ID      = 2  # 3. named tuples are not typed
-    LPAREN  = 3
-    RPAREN  = 4  # 4. class to represent a token
-    EOF = 5  # TODO return 
 
-    # fn - file name we are lexing
-    def __init__(self, fn:str):
+    # class variables that represent a code for a "kind" of token.
+    # TODO Clean this up so it is much shorter
+    INTLIT = 0        # 1) setattr builtin function
+    PLUS   = 1        # 2) namedtuple
+    ID     = 2        # 3) named tuples are not typed Typed Named Tuple in
+    LPAREN = 3        #     the typehints doc in Python
+    RPAREN = 4        # 4) Class to represent a token
+    EOF    = 5 # TODO return special end-of-file token
+    MULT   = 6
 
+    def __init__(self, fn: str):
         try:
             self.f = open(fn)
         except IOError:
             print("File {} not found".format(fn))
             print("Exiting")
-            sys.exit(1)  # cannot go on
+            sys.exit(1)  # can't go on
 
     def token_generator(self) -> Generator[Tuple[int, str], None, None]:
-        """
-        Returns tokens of the language
-        """
-        # backslash plague - eliminated because of python raw strings
-        split_patt = re.compile(r"(\+)|\s(\()|(\))")  # parentheses around a pattern captures the value
 
+        # TODO Can we make this more readable by putting this elsewhere?
+        # check out the documentation on |
+        # Don't forget about ^ and $
+        # TEST TEST TEST try and break your code
+        # SOLID
         split_patt = re.compile(
-            r""""           # Split on 
-            (\+)    |       # Plus and capture
-            (\*)    |
-            (-)     |       # minus and capture, minus not special unless in []
-            \s      |       # whitespace
-            (\()    |       # left paren and capture
-            (\))            # right paren and capture
+            r"""             # Split on 
+               (\+) |        #  plus and capture
+               (\*) |        #  times and capture
+               (-)  |        #  minus and capture, minus not special unless in []
+               \s   |        #  whitespace
+               (\() |        #  left paren and capture
+               (\))          #  right paren and capture
             """,
             re.VERBOSE
         )
 
+        # regular expression for an ID
+        # regular expression for an integer literal
+
         for line in self.f:
+
+            # save recognizing string literals and comments
+            # until the end (do these last). Try and recognize
+            # these *before* you split the line
+
             tokens = (t for t in split_patt.split(line) if t)
-            for t in tokens:        # repalce with a dictionary
+            for t in tokens:
+                # TODO replace with a dictionary
                 if t == '+':
-                    yield (Lexer.PLUS, t)
+                    yield (Lexer.PLUS, t)   # singleton
+                elif t == '*':
+                    yield (Lexer.MULT, t)
                 elif t == '(':
                     yield (Lexer.LPAREN, t)
                 elif t == ')':
                     yield (Lexer.RPAREN, t)
                 else:
-                    yield (Lexer.ID, t)
+                    yield (Lexer.ID, t)    # singleton?
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    lex = Lexer("test.sluc")
+    lex = Lexer("test.sluc")  # use command line arguments
 
     g = lex.token_generator()
 
