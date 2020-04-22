489844e5c8c72fc31bd86fed418fad35a53d48cb
xtao17 <xtao17@stlawu.edu>
2020-04-22 18:05:58 -0400

    function definition update

--- parser.py
+++ parser.py
@@ -67,12 +67,10 @@ class Parser:
                         self.currtok = next(self.tg)
                     if self.currtok.kind == "left-brace":
                         self.currtok = next(self.tg)
-                        while(self.currtok.name not in {"int", "bool", "float"}):
+                        while(self.currtok.name in {"int", "bool", "float"}):
                             decs.append(self.declaration())
-                            self.currtok=next(self.tg)
                         while(self.currtok.name != "}"):
                             stms.append(self.statement())
-                            self.currtok=next(self.tg)
 
 
                         if self.currtok.kind == "right-brace":
