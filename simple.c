78295848966c4449b77ca8b5c6848d78e5a6d09b
jzhan18 <mizar5528@gmail.com>
2020-04-24 11:53:02 -0400

    detect undefined variable

--- simple.c
+++ simple.c
@@ -3,8 +3,8 @@ int sum_3_or_5 (int n) {
 	int i;
 	sum = 0;
 	i = 0;
-		while (i < n) {
-			if(((i % 3 + 2 - 1) == 0) || ((i % 5) == 0))
+		while (i < n || sum < i) {
+			if(((sum % 3 + 2 - 1) == 0) || ((i % 5) == 0))
 				sum = (sum + i);
  				i = (i + 1);
 
