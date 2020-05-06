157b48f0df89c5774e0366636af679599bca1dc5
xtao17 <xtao17@stlawu.edu>
2020-05-06 15:59:43 -0400

    projectpart3 changes

--- simple.c
+++ simple.c
@@ -1,26 +1,7 @@
-int f(int x) {
-    return x*x;
-}
-
-int exp() {
-    return 99;
-}
-
-int main() {
-   int x;
-   float y;
-   bool z;
-   x = f(33);  // <-- Need to add function call syntax
-
-   // It is not hard to add function call syntax. It is just an identifier
-   // followed by zero or more expressions separated by commas.
-
-   y = x * 3.14;  // type checking. int * float is a float
-   x = 3.14;      // convert to an int by truncating
-
-
-   print(exp());
-
-
-   print(f());   // function composition should work
+int exp(int x, int y) {
+    print(y)
+    if (y == 0)
+        return 1;
+    else
+        return x*exp(x,y-1);
 }
