efa7d467a5e7291c48538b8d79d510069837e3fc
jzhan18 <mizar5528@gmail.com>
2020-05-07 21:35:11 -0400

    test

--- simple.c
+++ simple.c
@@ -1,31 +1,14 @@
-int f(int x) {
-    return x*x;
-}
-
-int exp(int x, int y) {
-    if (y == 0)
-        return 1;
-    else
-        return x*exp(x,y-1);
-}
-
-int a(int x, int y) {
-    if(y == 0)
-        return 1;
-    else
-        return x*a(x,y-1);
-}
 int main() {
-   int x=5;
+   int x;
    float y;
    bool z;
+   x = 33;
 
-   y = 5/2;
+   // It is not hard to add function call syntax. It is just an identifier
+   // followed by zero or more expressions separated by commas.
 
-   print(a(3,2));
-   print(x)
-   print("hello");
-   print("hi", a(4, 5));
-   print(3**2**3*2+4-5*2/2);
+   y = x * 3.14;  // type checking. int * float is a float
+   x = 3.14;      // convert to an int by truncating
+   z = true;
 
 }
